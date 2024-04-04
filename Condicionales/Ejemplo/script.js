c7693c90ba32b5531a15b25095d1139f85896c0f
Joshua-VVH <joshuamonsalvez@liceovvh.cl>
2024-04-04 12:35:00 -0300

    ok

---
+++ Condicionales/Ejemplo/script.js
@@ -0,0 +1,11 @@
+ function identificarMayorEdad(){
+    //Identificar si es mayor o menor de edad
+    let edad = prompt("Por favoringrese su edad: ");
+
+    if(edad >= 18){
+        alert(`Es mayor de edad- tiene: ${edad}`);
+    }
+    else{
+        alert(`Es menor  de edad- tiene: ${edad}`);
+    }
+ }
