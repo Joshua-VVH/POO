ca7644a49bad5a67c62be30c0c49d8e425a90551
Joshua-VVH <joshuamonsalvez@liceovvh.cl>
2024-04-04 12:50:17 -0300

    ok

--- Condicionales/Ejemplo/script.js
+++ Condicionales/Ejemplo/script.js
@@ -3,9 +3,9 @@
     let edad = prompt("Por favoringrese su edad: ");
 
     if(edad >= 18){
-        alert(`Es mayor de edad- tiene: ${edad}`);
+        alert(`Es mayor de edad, tiene: ${edad} años`);
     }
     else{
-        alert(`Es menor  de edad- tiene: ${edad}`);
+        alert(`Es menor  de edad, tiene: ${edad} años`);
     }
  }
