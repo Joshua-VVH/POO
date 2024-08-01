aad3d30089a9120aa0264aeb1aacda96838ed0c8
Joshua-VVH <joshuamonsalvez@liceovvh.cl>
2024-08-01 11:52:18 -0400

    ok

--- Encontrar_el_mayor/script.js
+++ Encontrar_el_mayor/script.js
@@ -1,20 +1,18 @@
 function encontrarMayorMenor(array){
+    
     let mayor = array[0];
+    let menor = array[0];
+    
     for(let i = 0; i < array.length; i++){
         if(array[i] > mayor){
             mayor = array[i]
         }
-    }
-    return mayor;    
-
-    let menor = array[0];
-    for(let i = 0; i < array.length; i++){
-        if(array[i] < menor){
+        else if(array[i] < menor){
             menor = array[i]
         }
     }
-    return menor; 
+    alert("El numero mayor es: " + mayor + "\nel numero menor es: " + menor);
+    return array; 
 };
 
-let resultado = encontrarMayorMenor([3,5,7,2,200,33,100,3]);
-alert(resultado); 
+let resultado = encontrarMayorMenor([3,5,10]);
